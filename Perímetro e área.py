ae02e7a2735f1cda89edd69e3a3831d16e3b7d7b
Letícia Borges <97967025+leticiapbrs@users.noreply.github.com>
2023-03-30 22:48:06 -0300

    Delete Perímetro e área.py

--- Perímetro e área.py
+++
@@ -1,4 +0,0 @@
-ladoQuadrado = int(input("Digite o valor correspondente ao lado de um quadrado: "))
-perimetro = 4 * ladoQuadrado
-area = ladoQuadrado * ladoQuadrado
-print("perímetro:", perimetro, "- área:", area)
