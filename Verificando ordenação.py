78166264687f104d687885a092a8f6706d8fd612
Letícia Borges <97967025+leticiapbrs@users.noreply.github.com>
2023-03-30 22:48:12 -0300

    Delete Verificando ordenação.py

--- Verificando ordenação.py
+++
@@ -1,8 +0,0 @@
-numero1 = int(input("Insira o primeiro número inteiro: "))
-numero2 = int(input("Insira o segundo número inteiro: "))
-numero3 = int(input("Insira o terceiro número inteiro: "))
-
-if numero1 < numero2 and numero2 < numero3:
-        print("crescente")
-else:
-        print("não está em ordem crescente")
